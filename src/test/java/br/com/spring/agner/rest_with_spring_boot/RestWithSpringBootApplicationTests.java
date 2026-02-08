2436b20993f86c1f3ad81e38ed68198e01e86032
Agner Bezerra <agner.bezerra@hotmail.com>
2026-02-07 22:43:13 -0300

    First commit

---
+++ src/test/java/br/com/spring/agner/rest_with_spring_boot/RestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.agner.rest_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
