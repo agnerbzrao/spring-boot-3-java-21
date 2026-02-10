0ffdba5b70ad26b8b6a637dabc34b639301fc191
Agner Bezerra <agner.bezerra@hotmail.com>
2026-02-10 17:50:27 -0300

    Add the ObjectMapperTests, ObjectMapper and MockPerson tests classes.

---
+++ src/test/java/br/com/spring/agner/rest_with_spring_boot/unit/tests/maper/mocks/MockPerson.java
@@ -0,0 +1,57 @@
+package br.com.spring.agner.rest_with_spring_boot.unit.tests.maper.mocks;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.spring.agner.rest_with_spring_boot.data.dto.PersonDTO;
+import br.com.spring.agner.rest_with_spring_boot.model.PersonModel;
+
+public class MockPerson {
+
+
+    public PersonModel mockEntity() {
+        return mockEntity(0);
+    }
+
+    public PersonDTO mockDTO() {
+        return mockDTO(0);
+    }
+
+    public List<PersonModel> mockEntityList() {
+        List<PersonModel> persons = new ArrayList<PersonModel>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockEntity(i));
+        }
+        return persons;
+    }
+
+    public List<PersonDTO> mockDTOList() {
+        List<PersonDTO> persons = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockDTO(i));
+        }
+        return persons;
+    }
+
+    public PersonModel mockEntity(Integer number) {
+        PersonModel person = new PersonModel();
+        person.setAddress("Address Test" + number);
+        person.setFirstName("First Name Test" + number);
+        person.setGender(((number % 2) == 0) ? "Male" : "Female");
+        person.setId(number.longValue());
+        person.setLastName("Last Name Test" + number);
+        return person;
+    }
+
+    public PersonDTO mockDTO(Integer number) {
+        PersonDTO person = new PersonDTO();
+        person.setAddress("Address Test" + number);
+        person.setFirstName("First Name Test" + number);
+        person.setGender(((number % 2) == 0) ? "Male" : "Female");
+        person.setId(number.longValue());
+        person.setLastName("Last Name Test" + number);
+        return person;
+    }
+
+}
