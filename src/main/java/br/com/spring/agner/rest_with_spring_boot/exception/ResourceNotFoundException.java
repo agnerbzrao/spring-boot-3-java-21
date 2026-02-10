a69a58364feba3573bd579700b3a5a2ef1270d7f
Agner Bezerra <agner.bezerra@hotmail.com>
2026-02-10 11:27:03 -0300

    Create the ResourceNotFoundException class exception.

---
+++ src/main/java/br/com/spring/agner/rest_with_spring_boot/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package br.com.spring.agner.rest_with_spring_boot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
