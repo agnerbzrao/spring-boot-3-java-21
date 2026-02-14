fcbab4182b6ba2bbdf013269c00bbd5e07dc845e
Agner Bezerra <agner.bezerra@hotmail.com>
2026-02-14 20:59:31 -0300

    Make adjust in the folder name.

--- src/main/java/br.com.spring.agner.rest_with_spring_boot/controllers/PersonController.java
+++ src/main/java/br.com.spring.agner.rest_with_spring_boot/controllers/PersonController.java
@@ -2,12 +2,14 @@ package br.com.spring.agner.rest_with_spring_boot.controllers;
 
 import br.com.spring.agner.rest_with_spring_boot.data.dto.v1.PersonDTO;
 import br.com.spring.agner.rest_with_spring_boot.data.dto.v2.PersonDTOV2;
+import br.com.spring.agner.rest_with_spring_boot.model.PersonModel;
 import br.com.spring.agner.rest_with_spring_boot.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -22,9 +24,13 @@ public class PersonController {
         return personService.findByAll();
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public PersonDTO findById(@PathVariable("id") Long id) {
-        return personService.findById(id);
+        PersonDTO personDTO = personService.findById(id);
+        personDTO.setBirthDay(new Date());
+        personDTO.setPhoneNumber("1111111");
+        personDTO.setSensitiveDate("Foo Bar");
+        return personDTO;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -32,7 +38,7 @@ public class PersonController {
         return personService.create(personDTO);
     }
 
-    @PostMapping(value="/v2", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/v2", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public PersonDTOV2 createV2(@RequestBody PersonDTOV2 personDTOV2) {
         return personService.createV2(personDTOV2);
     }
