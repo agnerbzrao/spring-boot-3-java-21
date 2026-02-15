e32e7c138177b9dbe2e1787695368a196764b159
Agner Bezerra <agner.bezerra@hotmail.com>
2026-02-14 21:03:08 -0300

    Create the ObjectMapperConfig class.

---
+++ src/main/java/br.com.spring.agner.rest_with_spring_boot/config/ObjectMapperConfig.java
@@ -0,0 +1,23 @@
+package br.com.spring.agner.rest_with_spring_boot.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleFilterProvider filter = new SimpleFilterProvider()
+                .addFilter("PersonFilter",
+                        SimpleBeanPropertyFilter.
+                                serializeAllExcept(
+                                        "sensitiveDate"));
+        mapper.setFilterProvider(filter);
+        return mapper;
+    }
+}
