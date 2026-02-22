13758da1eb40aec2e5bf41fee74484c45b5f4df6
Agner Souza Bezerra <agnerbzrao@hotmail.com>
2026-02-22 15:24:19 -0300

    Add the new JavaTimeModule() instance to handle with localDateTime.

--- src/main/java/br.com.spring.agner.rest_with_spring_boot/serializer/YamlJackson2HttpMessageConverter.java
+++ src/main/java/br.com.spring.agner.rest_with_spring_boot/serializer/YamlJackson2HttpMessageConverter.java
@@ -1,6 +1,7 @@
 package br.com.spring.agner.rest_with_spring_boot.serializer;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
@@ -8,6 +9,7 @@ import org.springframework.http.converter.json.AbstractJackson2HttpMessageConver
 public final class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
     protected YamlJackson2HttpMessageConverter() {
         super(new YAMLMapper()
+                .registerModule(new JavaTimeModule())
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL),
                 MediaType.parseMediaType("application/x-yaml"));
     }
