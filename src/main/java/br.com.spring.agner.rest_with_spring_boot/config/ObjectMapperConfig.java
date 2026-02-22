359458a73b9fe6eeb7b2865862ce5951559f50ca
Agner Souza Bezerra <agnerbzrao@hotmail.com>
2026-02-22 15:20:55 -0300

    Add the new JavaTimeModule() instance to handle the localDateTime.

--- src/main/java/br.com.spring.agner.rest_with_spring_boot/config/ObjectMapperConfig.java
+++ src/main/java/br.com.spring.agner.rest_with_spring_boot/config/ObjectMapperConfig.java
@@ -3,6 +3,7 @@ package br.com.spring.agner.rest_with_spring_boot.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ public class ObjectMapperConfig {
     @Bean
     public ObjectMapper objectMapper(){
         ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
         SimpleFilterProvider filter = new SimpleFilterProvider()
                 .addFilter("PersonFilter",
                         SimpleBeanPropertyFilter.
