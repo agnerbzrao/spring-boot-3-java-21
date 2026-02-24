a0756305b1a73be9fc45707534379c787972ba9a
Agner Souza Bezerra <agnerbzrao@hotmail.com>
2026-02-23 21:28:51 -0300

    Add the TestConfigs interface.

---
+++ src/test/java/br/com/spring/agner/rest_with_spring_boot/config/TestConfigs.java
@@ -0,0 +1,10 @@
+package br.com.spring.agner.rest_with_spring_boot.config;
+
+public interface TestConfigs {
+    int SERVER_PORT = 8888;
+
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+    String ORIGIN_LOCAL = "http://localhost:8080";
+    String ORIGIN_UNKNOWN = "unknown";
+}
