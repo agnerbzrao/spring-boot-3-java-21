872a6d01b7e190fe543c950459457f106d357033
Agner Souza Bezerra <agnerbzrao@hotmail.com>
2026-02-25 15:05:36 -0300

    Add the disablePerson docs method.

--- src/main/java/br.com.spring.agner.rest_with_spring_boot/controllers/docs/PersonControllerDocs.java
+++ src/main/java/br.com.spring.agner.rest_with_spring_boot/controllers/docs/PersonControllerDocs.java
@@ -89,8 +89,26 @@ public interface PersonControllerDocs {
     )
     PersonDTO update(@RequestBody PersonDTO person);
 
-    @Operation(summary = "Deletes a Person",
-            description = "Deletes a specific person by their ID",
+    @Operation(summary = "Finds a Person",
+            description = "Find a specific person by your ID",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PersonDTO.class))
+                    ),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    PersonDTO disablePerson(@PathVariable("id") Long id);
+
+    @Operation(summary = "Disable a Person",
+            description = "Disable a specific person by their ID",
             tags = {"People"},
             responses = {
                     @ApiResponse(
