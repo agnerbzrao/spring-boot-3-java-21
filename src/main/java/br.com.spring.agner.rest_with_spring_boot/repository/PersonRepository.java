f8a61288c61b4a99ae00f7c085cea828698b6d3b
Agner Souza Bezerra <agnerbzrao@hotmail.com>
2026-02-25 15:06:52 -0300

    Add the disablePerson method.

--- src/main/java/br.com.spring.agner.rest_with_spring_boot/repository/PersonRepository.java
+++ src/main/java/br.com.spring.agner.rest_with_spring_boot/repository/PersonRepository.java
@@ -2,6 +2,13 @@ package br.com.spring.agner.rest_with_spring_boot.repository;
 
 import br.com.spring.agner.rest_with_spring_boot.model.PersonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PersonRepository extends JpaRepository<PersonModel, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE PersonModel p SET p.enabled = false WHERE p.id =:id")
+    void disablePerson(@Param("id") Long id);
 }
