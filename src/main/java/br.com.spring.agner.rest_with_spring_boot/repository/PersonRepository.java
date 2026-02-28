c64c8b0ad504627895b4f53e0c4781bf01a3c23d
Agner Souza Bezerra <agnerbzrao@hotmail.com>
2026-02-28 19:55:54 -0300

    Add the findPeopleByName method.

--- src/main/java/br.com.spring.agner.rest_with_spring_boot/repository/PersonRepository.java
+++ src/main/java/br.com.spring.agner.rest_with_spring_boot/repository/PersonRepository.java
@@ -1,6 +1,8 @@
 package br.com.spring.agner.rest_with_spring_boot.repository;
 
 import br.com.spring.agner.rest_with_spring_boot.model.PersonModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +13,7 @@ public interface PersonRepository extends JpaRepository<PersonModel, Long> {
     @Modifying(clearAutomatically = true)
     @Query("UPDATE PersonModel p SET p.enabled = false WHERE p.id =:id")
     void disablePerson(@Param("id") Long id);
+
+    @Query("SELECT p FROM PersonModel p WHERE p.firstName LIKE LOWER(CONCAT ('%',:firstName,'%'))")
+    Page<PersonModel> findPeopleByName(@Param("firstName") String firstName, Pageable pageable);
 }
