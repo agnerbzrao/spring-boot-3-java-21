fa1a6d2bea61ff0e9a923c4f1ff53737e8ac7c5b
Agner Souza Bezerra <agnerbzrao@hotmail.com>
2026-03-02 11:05:59 -0300

    Add the findByName test.

--- src/test/java/br/com/spring/agner/rest_with_spring_boot/integrationstest/controllers/withxml/PersonControllerXmlTest.java
+++ src/test/java/br/com/spring/agner/rest_with_spring_boot/integrationstest/controllers/withxml/PersonControllerXmlTest.java
@@ -203,6 +203,48 @@ public class PersonControllerXmlTest extends AbstractIntegrationTest {
         assertEquals("Female", personFour.getGender());
         assertTrue(personFour.getEnabled());
     }
+
+    @Test
+    @Order(7)
+    void findByName() throws JsonProcessingException {
+
+        String content = given(specification)
+                .accept(MediaType.APPLICATION_XML_VALUE)
+                .pathParam("firstName", "and")
+                .queryParam("page", 0,"size",12,"direction", "asc")
+                .when()
+                .get("findPeopleByName/{firstName}")
+                .then()
+                .statusCode(200)
+                .contentType(MediaType.APPLICATION_XML_VALUE)
+                .extract()
+                .body()
+                .asString();
+
+        PagedModelPerson wrapper = xmlMapper.readValue(content, PagedModelPerson.class);
+        List<PersonDTO> people = wrapper.getContent();
+        PersonDTO personOne = people.getFirst();
+
+        assertNotNull(personOne.getId());
+        assertTrue(personOne.getId() > 0);
+
+        assertEquals("Alessandro", personOne.getFirstName());
+        assertEquals("McFaul", personOne.getLastName());
+        assertEquals("5 Lukken Plaza", personOne.getAddress());
+        assertEquals("Male", personOne.getGender());
+        assertTrue(personOne.getEnabled());
+
+        PersonDTO personFour = people.get(4);
+
+        assertNotNull(personFour.getId());
+        assertTrue(personFour.getId() > 0);
+
+        assertEquals("Brandyn", personFour.getFirstName());
+        assertEquals("Grasha", personFour.getLastName());
+        assertEquals("96 Mosinee Parkway", personFour.getAddress());
+        assertEquals("Male", personFour.getGender());
+        assertTrue(personFour.getEnabled());
+    }
     private void mockPerson() {
         personDTO.setFirstName("John");
         personDTO.setLastName("Borner");
