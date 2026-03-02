4cddd058b3fbe130a926b8cdb33ea470051a04db
Agner Souza Bezerra <agnerbzrao@hotmail.com>
2026-03-02 11:08:45 -0300

    Add the handleFileNotFoundExceptions and handleFileStorageExceptions in order to handle with exceptions when upload or download file.

--- src/main/java/br.com.spring.agner.rest_with_spring_boot/exception/handler/CustomEntityResponseHandler.java
+++ src/main/java/br.com.spring.agner.rest_with_spring_boot/exception/handler/CustomEntityResponseHandler.java
@@ -1,8 +1,6 @@
 package br.com.spring.agner.rest_with_spring_boot.exception.handler;
 
-import br.com.spring.agner.rest_with_spring_boot.exception.ExceptionResponse;
-import br.com.spring.agner.rest_with_spring_boot.exception.ResourceNotFoundException;
-import br.com.spring.agner.rest_with_spring_boot.exception.ResourceObjectIsNullException;
+import br.com.spring.agner.rest_with_spring_boot.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -40,5 +38,23 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
                 ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleFileNotFoundExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(FileStorageException.class)
+    public final ResponseEntity<ExceptionResponse> handleFileStorageExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
