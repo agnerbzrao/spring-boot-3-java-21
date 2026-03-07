eedccf7b1c9abe266594139fb97e5f10c9d21067
Agner Souza Bezerra <agnerbzrao@hotmail.com>
2026-03-07 20:33:47 -0300

    Add the FileImporter interface.

---
+++ src/main/java/br.com.spring.agner.rest_with_spring_boot/file/importer/contract/FileImporter.java
@@ -0,0 +1,11 @@
+package br.com.spring.agner.rest_with_spring_boot.file.importer.contract;
+
+import br.com.spring.agner.rest_with_spring_boot.data.dto.v1.PersonDTO;
+
+import java.io.InputStream;
+import java.util.List;
+
+public interface FileImporter {
+
+    List<PersonDTO> importFile(InputStream inputStream) throws Exception;
+}
