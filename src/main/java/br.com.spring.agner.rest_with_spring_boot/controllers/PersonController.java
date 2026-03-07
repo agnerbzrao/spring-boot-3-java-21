be1a01edae3ce6b544ba236426fa01509810b7ed
Agner Souza Bezerra <agnerbzrao@hotmail.com>
2026-03-07 20:36:22 -0300

    Add the exportPage and massCreation methods.

--- src/main/java/br.com.spring.agner.rest_with_spring_boot/controllers/PersonController.java
+++ src/main/java/br.com.spring.agner.rest_with_spring_boot/controllers/PersonController.java
@@ -3,17 +3,22 @@ package br.com.spring.agner.rest_with_spring_boot.controllers;
 import br.com.spring.agner.rest_with_spring_boot.controllers.docs.PersonControllerDocs;
 import br.com.spring.agner.rest_with_spring_boot.data.dto.v1.PersonDTO;
 import br.com.spring.agner.rest_with_spring_boot.data.dto.v2.PersonDTOV2;
+import br.com.spring.agner.rest_with_spring_boot.file.exporter.MediaTypes;
 import br.com.spring.agner.rest_with_spring_boot.services.PersonService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Date;
 import java.util.List;
@@ -36,6 +41,35 @@ public class PersonController implements PersonControllerDocs {
         Pageable pageable = PageRequest.of(page, size, Sort.by(shortDirection, "firstName"));
         return ResponseEntity.ok(personService.findAll(pageable));
     }
+
+    @GetMapping(value = "/exportPage", produces = {MediaTypes.APPLICATION_CSV_VALUE, MediaTypes.APPLICATION_XLSX_VALUE})
+    @Override
+    public ResponseEntity<Resource> exportPage(
+            @RequestParam(value = "page", defaultValue = "1") Integer page,
+            @RequestParam(value = "size", defaultValue = "12") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction,
+            HttpServletRequest httpServletRequest
+    ) {
+        Sort.Direction sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
+
+        String acceptHeader = httpServletRequest.getHeader(HttpHeaders.ACCEPT);
+
+        Resource file = personService.exportPage(pageable, acceptHeader);
+
+
+        String contentType = acceptHeader != null ? acceptHeader : "application/octet-stream";
+        String fileExtension = MediaTypes.APPLICATION_XLSX_VALUE.equalsIgnoreCase(acceptHeader) ? ".xlsx" : ".csv";
+        String filename = "people_exported" + fileExtension;
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(
+                        HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + filename + "\"")
+                .body(file);
+    }
+
 
     @Override
     @GetMapping(value = "/findPeopleByName/{firstName}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_YAML_VALUE, MediaType.APPLICATION_XML_VALUE})
@@ -64,6 +98,18 @@ public class PersonController implements PersonControllerDocs {
     public PersonDTO create(@RequestBody PersonDTO personDTO) {
         return personService.create(personDTO);
     }
+
+    @PostMapping(value = "/massCreation",
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE}
+    )
+    @Override
+    public List<PersonDTO> massCreation(@RequestParam("file") MultipartFile file) throws BadRequestException {
+        return personService.massCreation(file);
+    }
+
 
 
     @PostMapping(value = "/v2", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_YAML_VALUE, MediaType.APPLICATION_XML_VALUE})
