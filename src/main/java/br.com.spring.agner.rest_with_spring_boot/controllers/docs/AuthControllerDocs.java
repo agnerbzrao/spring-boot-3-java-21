a1b63b81f2c8238950c1c81271bd5c1965caf0cd
Agner Souza Bezerra <agnerbzrao@hotmail.com>
2026-03-16 10:59:50 -0300

    Add the tag content on create new user method.

--- src/main/java/br.com.spring.agner.rest_with_spring_boot/controllers/docs/AuthControllerDocs.java
+++ src/main/java/br.com.spring.agner.rest_with_spring_boot/controllers/docs/AuthControllerDocs.java
@@ -1,9 +1,13 @@
 package br.com.spring.agner.rest_with_spring_boot.controllers.docs;
 
 import br.com.spring.agner.rest_with_spring_boot.data.dto.security.AccountCredentialsDTO;
+import br.com.spring.agner.rest_with_spring_boot.data.dto.v1.PersonDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 public interface AuthControllerDocs {
@@ -41,7 +45,12 @@ public interface AuthControllerDocs {
             description = "Registers a new user in the system with the provided credentials.",
             tags = {"User Management"},
             responses = {
-                    @ApiResponse(description = "Created", responseCode = "201", content = @Content),
+                    @ApiResponse(description = "Created", responseCode = "201", content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = AccountCredentialsDTO.class))
+                            )
+                    }),
                     @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
                     @ApiResponse(description = "Conflict", responseCode = "409", content = @Content),
                     @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
